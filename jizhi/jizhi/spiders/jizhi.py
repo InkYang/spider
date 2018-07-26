97c8bea6ea3c88ef8cc3a0eb1233b717b6c502e4
Yang <InkYang@users.noreply.github.com>
2018-07-26 09:12:43 +0800

    Initial commit

---
+++ jizhi/jizhi/spiders/jizhi.py
@@ -0,0 +1,41 @@
+import scrapy
+import sys
+
+class Jizhi(scrapy.Spider):
+
+    name = 'jizhi'
+
+    arg = ' '.join(sys.argv[3:])
+
+    base_urls = "http://www.baidu.com"
+
+    start_urls = [
+        'http://www.baidu.com/s?{}'.format(arg)
+    ]
+
+    #counter = 0
+
+    def parse(self, response):
+
+        result_lst = response.css("h3.t a::text")
+
+        for result in result_lst:
+            yield {
+                'title':result.extract()
+            }
+
+        #Jizhi.counter +=1
+
+        # next_page:css('a.n')
+
+        #if Jizhi.counter == 5: return        
+
+        #yield response.css('a.n::attr(href)')
+'''
+        if len(next_page) == 2:
+            next_page_url = next_page[1].extract()
+        else:
+            next_page_url = next_page.extract_first()
+
+        yield scrapy.Request(self.base_urls + next_page_url, callback=self.parse)
+'''
